cbdfe148df48b546c014738bd1b9017ba82af16b
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-09-15 13:09:13 +0700

    update

--- main.go
+++ main.go
@@ -6,9 +6,35 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"time"
+	"github.com/A-Engine/fake-plugin-active/client"
 )
 
+var count = 0
+
+func download(){
+	wporg.UpdateCheck()
+	wporg.DownloadPlugin()
+
+	ticker := time.NewTicker(5 * time.Second)
+	quit := make(chan bool)
+	for {
+		select {
+		case  <- ticker.C:
+			wporg.UpdateCheck()
+			wporg.DownloadPlugin()
+			count++
+		case <- quit:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
 func main() {
+
+	go download()
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -21,7 +47,9 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"count": count,
+		})
 	})
 
 	router.Run(":" + port)
